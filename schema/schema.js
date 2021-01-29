4dd4a6f2e6328b104a992ffecbdbc6a42d1e71b8
Caitlin Watson-Beaton <c.watsonbeaton@gmail.com>
2021-01-29 11:58:01 +1000

    addUser mutation; NonNull arg validation

--- schema/schema.js
+++ schema/schema.js
@@ -5,6 +5,7 @@ const {
 	GraphQLString,
 	GraphQLInt,
 	GraphQLList,
+	GraphQLNonNull,
 	GraphQLSchema
 } = graphql;
 
@@ -63,6 +64,26 @@ const RootQuery = new GraphQLObjectType({
 	}
 });
 
+const mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		addUser: {
+			type: UserType,
+			args: {
+				firstName: { type: new GraphQLNonNull(GraphQLString) },
+				age: { type: new GraphQLNonNull(GraphQLInt) },
+				companyId: { type: GraphQLString }
+			},
+			resolve(parentValue, { firstName, age }) {
+				console.log('POSTING', firstName, age);
+				return axios.post(`http://localhost:3000/users`, { firstName, age })
+					.then(res => res.data);
+			}
+		}
+	}
+});
+
 module.exports = new GraphQLSchema({
-	query: RootQuery
+	query: RootQuery,
+	mutation
 });
